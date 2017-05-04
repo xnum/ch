afcf72fe99aa1ab79023625dfcf95579763560c2
xnum <s000032001@gmail.com>
2017-05-04 16:47:55 +0800

    Init

---
+++ README.md
@@ -0,0 +1,37 @@
+# chh (Change Home)
+
+## User Story
+
+某一天，我在做一個逆向工程分析，結束後留下許多垃圾代碼和抓下來的工具軟體，想著以後都不會再用到了而一個一個刪掉他們，這時候我在想如果有一個工具可以隔離這些檔案就好了，於是我就自幹了一個...
+
+## Introduction
+
+因為我很懶惰，操作Terminal喜歡切到家目錄做任何事情，久而久之在家目錄堆積了一堆檔案。這個工具幫我建立一個虛擬的家目錄，然後我就眼不見為淨了！！！
+
+此外程式還會幫你把所有在家目錄的隱藏檔例如`.vimrc .bashrc .zshrc .tmux.info`全都複製進去，所以就Terminal Tools的設定仍會是正常的
+
+除非你在設定檔裡面用到`~`，lmao
+
+複製操作使用`rsync` 參數`-auq`
+
+## Installation
+
+記住在過程中不要用到`~`作為你的位置，因為在程式執行後會代換掉`~`的實際位置
+
+1. 下載本程式 `chh.sh`
+2. `chmod +x chh.sh`
+3. 放到一個你喜歡的地方，例如我放在`/home/num/chh`
+4. 修改你的shellrc，例如我用zsh，就修改~/.zshrc。 加上`alias chh='source /home/num/chh/chh.sh'`
+5. 重開shell或`source ~/.zshrc` 
+
+## Usage
+
+`$ chh new test` 新建一個`test`環境
+`$ chh load test` 載入`test`環境 把家目錄切換過去
+`$ chh exit` 離開它 回到你溫暖的家
+
+## TODO
+
+- [ ] 不使用rsync強制覆蓋，用hard link節省空間
+- [ ] 自動化安裝
+- [ ] 看起來超專業的Usage和Error message
---
+++ chh.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+REAL_HOME=$(cat /etc/passwd | grep $USER | awk -F ':' '{ print $6 }')
+WORK_DIR="$REAL_HOME/.xnws"
+
+load_ws() {
+    if [ ! -d "$WORK_DIR/$1" ] ; then
+        echo "Error: workspace [$1] is not exists"
+        return
+    fi
+
+    export ORIG_PS1="$PS1"
+    export PS1="[$1] $PS1"
+    export HOME="$WORK_DIR/$1"
+
+    rsync -auq --exclude ".xnws" $REAL_HOME/.* $HOME
+    cd ~
+}
+
+exit_ws() {
+    export PS1=$ORIG_PS1
+    export HOME=$REAL_HOME
+    cd ~
+}
+
+create_ws() {
+    if [ ! -d $WORK_DIR ] ; then
+        mkdir $WORK_DIR
+    fi
+
+    if [ ! -d "$WORK_DIR/$1" ] ; then
+        mkdir $WORK_DIR/$1
+    fi
+
+    load_ws $1
+}
+
+usage() {
+    echo "usage: chh (new|load|exit) [name]"
+    echo "\tnew [name]"
+    echo "\tload [name]"
+    echo "\texit"
+}
+
+case "$1" in
+new)
+    if [ $# -ne 2 ]; then
+        usage
+    else
+        create_ws $2
+    fi
+    ;;
+
+load)
+    if [ "$REAL_HOME" != "$HOME" ] ; then
+        echo "Error: You are at virtual home"
+    else
+        if [ $# -ne 2 ]; then
+            usage
+        else
+            load_ws $2
+        fi
+    fi
+    ;;
+
+exit)
+    if [ "$REAL_HOME" = "$HOME" ] ; then
+        echo "Error: You are at real home"
+    else
+        exit_ws
+    fi
+    ;;
+*)
+    usage
+
+    ;;
+esac
+
